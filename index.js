b80f3870cc6fd6f6cb20e4b009b9c4c5011a5b3c
Sultan Mahmud <smrsaimon960@gmail.com>
2021-03-23 16:01:31 +0600

    dynamic Id

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require("express")
+const express = require("express");
 app = express();
 
 
+app.get("/user/info", (req, res) => {
 
-app.get('/', function (req, res) {
-  res.send("I am from mode js ")
+  const user = {
+    name: "saimon",
+    id: 1,
+    job: "No"
+  }
+  res.send(user)
 })
 
-app.listen(440, function () {
-  console.log("surver run success!")
+
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params)
+})
+
+
+app.listen(4030, () => {
+  console.log("surver successfully")
 })
