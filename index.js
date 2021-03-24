06a55cb66a3f3be5223dce2dd13d832a22c84d8e
Sultan Mahmud <smrsaimon960@gmail.com>
2021-03-24 13:22:50 +0600

    Done

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
-app = express();
+const app = express();
+const cors = require("cors")
+const bodyParser = require('body-parser')
 
 
-app.get("/user/info", (req, res) => {
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-  const user = {
-    name: "saimon",
-    id: 1,
-    job: "No"
-  }
+
+const user = {
+  name: "saimon",
+  age: 23,
+  job: "No Job"
+}
+
+const persons = ["kodu", "Modu", "lalMia", "kalaMia", "chad Mia "]
+
+
+app.get("/", (req, res) => {
   res.send(user)
 })
 
-
-
-app.get("/users/:id", (req, res) => {
-  console.log(req.params)
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const name = persons[id];
+  res.send({ id, name })
 })
 
+app.post("/addInfo", (req, res) => {
 
-app.listen(4030, () => {
-  console.log("surver successfully")
+  const user = req.body
+  user.id = 55;
+  res.send(user)
+
+})
+app.listen(4011, () => {
+  console.log("Server Response Successfully Completed")
 })
